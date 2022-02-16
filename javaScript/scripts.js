508d9cc5cfd3145e6de782c93e866084c7525734
João Sarmento <joaovicentesarm@gmail.com>
2022-02-16 13:59:29 -0300

    Pegando os Quizzes da API

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -1,6 +1,38 @@
 // Constante Global
 
-const BUZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+const BUZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
+let Quizzes= [];
+
+getQuizzes();
+
+// Função para receber quizzes da API
+function getQuizzes(){
+    const request = axios.get(BUZZ_API + 'quizzes');
+    request.then(CarregarQuizzes);
+    request.catch();
+}
+
+// Função para carregar os Quizzes no array
+function CarregarQuizzes(message){
+    Quizzes = message.data;
+    console.log(Quizzes);
+    MostrarQuizzes();
+}
+
+// Função para mostrar os Quizzes na tela 
+function MostrarQuizzes(){
+    const AllQuizzes = document.querySelector('.other-quizzes');
+    for(contador=0;contador<Quizzes.length;contador++){
+        AllQuizzes.innerHTML += `
+        <div class="box">
+            <div class="title">
+                <span>${Quizzes[contador].title}</span>
+            </div>
+        </div>`
+    }
+}
+
+
 
 // Função para ir para a página de criar Quizzes
 function CreateQuizz(){
