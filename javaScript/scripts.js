5dc0c2080eb124ad3e5dc981d66b0587a3afdfba
João Sarmento <joaovicentesarm@gmail.com>
2022-02-15 22:20:02 -0300

    Função para abrir a página de criar Quizzes

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -1,3 +1,9 @@
 // Constante Global
 
 const BUZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+
+// Função para ir para a página de criar Quizzes
+function CreateQuizz(){
+    document.querySelector('.main-page').classList.add('hidden');
+    document.querySelector('.creation').classList.remove('hidden');
+}
