52a3c0421ce2cf5893fae7d24d79c2e8377ff64f
Jackie-Moraes <patrickdeandradecardosomoraes@gmail.com>
2022-02-16 18:16:44 -0300

    Gerando Perguntas dinâmicamente

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -1,10 +1,19 @@
-// Constante Global
+// Constante Global - API
 
 const BUZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/";
+
+// Variáveis Globais - Gerais
 let Quizzes= [];
 let gradient = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)"
 
-getQuizzes();
+
+// Variáveis Globais - Criação de Quizz
+
+let quizzTitle = "";
+let quizzUrl = "";
+let quizzQuestions = 0;
+let quizzLevels = 0;
+
 
 // Função para receber quizzes da API
 function getQuizzes(){
@@ -23,7 +32,7 @@ function CarregarQuizzes(message){
 // Função para mostrar os Quizzes na tela 
 function MostrarQuizzes(){
     const AllQuizzes = document.querySelector('.other-quizzes');
-    AllQuizzes.innerHTML = "";
+    AllQuizzes.innerHTML = "<h2>Todos os Quizzes</h2>";
     let selecionarImagem = null;
     for(contador=0;contador<Quizzes.length;contador++){
         const id = Quizzes[contador].id;
@@ -58,15 +67,13 @@ function CreateQuizz(){
     document.querySelector('.info-quizz').classList.remove('hidden');
 }
 
-function createQuestions() {
+function verifyQuestions() {
     
     let inputLocation = document.querySelectorAll('.info-quizz input')
     let inputVerifierCounter = 0;
 
     for (let i = 0; i < inputLocation.length; i++) {
-        if(!inputLocation[i].checkValidity()) {
-            alert(`Preencha o campo "${inputLocation[i].name}" corretamente.`)
-        } else {
+        if(inputLocation[i].checkValidity()) {
             inputVerifierCounter++;
         }
     }
@@ -74,18 +81,21 @@ function createQuestions() {
     if (inputVerifierCounter === inputLocation.length) {
         document.querySelector('.info-quizz').classList.add('hidden');
         document.querySelector('.questions-quizz').classList.remove('hidden');
+        quizzTitle = inputLocation[0].value;
+        quizzUrl = inputLocation[1].value;
+        quizzQuestions = inputLocation[2].value;
+        quizzLevels = inputLocation[3].value;
+        generateQuestions();
     }
 }
 
-function createLevels() {
+function verifyLevels() {
 
     let inputLocation = document.querySelectorAll('.questions-quizz input')
     let inputVerifierCounter = 0;
 
     for (let i = 0; i < inputLocation.length; i++) {
-        if (!inputLocation[i].checkValidity()) {
-            alert(`Preencha o campo "${inputLocation[i].name}" corretamente.`)
-        } else {
+        if (inputLocation[i].checkValidity()) {
             inputVerifierCounter++;
         }
     }
@@ -96,15 +106,13 @@ function createLevels() {
     }
 }
 
-function finishQuizz() {
+function verifyFinishQuizz() {
 
     let inputLocation = document.querySelectorAll('.levels-quizz input')
     let inputVerifierCounter = 0;
 
     for (let i = 0; i < inputLocation.length; i++) {
-        if (!inputLocation[i].checkValidity()) {
-            alert(`Preencha o campo "${inputLocation[i].name}" corretamente.`)
-        } else {
+        if (inputLocation[i].checkValidity()) {
             inputVerifierCounter++;
         }
     }
@@ -120,3 +128,47 @@ function backHome() {
     document.querySelector('.finish-quizz').classList.add('hidden');
     document.querySelector('.main-page').classList.remove('hidden');
 }
+
+function generateQuestions() {
+    const questionsLocale = document.querySelector('.questions-quizz');
+    questionsLocale.innerHTML = "<span><strong>Crie suas perguntas</strong></span>";
+
+    for (let i = 1; i <= quizzQuestions; i++) {
+        questionsLocale.innerHTML += `
+            <div class="container">
+
+                <span><strong>Pergunta ${i}</strong></span>
+                <div class="setup questions">
+                    <input type="text" name="Texto da Pergunta ${i}" required minlength="20" placeholder="Texto da pergunta">
+                    <input type="text" name="Cor da Pergunta ${i}" required maxlength="7" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" placeholder="Cor de fundo da pergunta">
+                </div>
+
+                <span><strong>Resposta correta</strong></span>
+                <div class="setup questions">
+                    <input type="text" name="Resposta Correta (Pergunta ${i})" required placeholder="Resposta correta">
+                    <input type="url" name="URL da Imagem Correta (Pergunta ${i})" required placeholder="URL da imagem">
+                </div>
+
+                <span><strong>Respostas incorretas</strong></span>
+                <div class="setup questions">
+                    <input type="text" name="Resposta Incorreta 1 (Pergunta ${i})" required placeholder="Resposta incorreta 1">
+                    <input type="url" name="URL da Imagem Incorreta 1 (Pergunta ${i})" required placeholder="URL da imagem 1">
+                </div>
+
+                <div class="setup questions">
+                    <input type="text" name="Resposta Incorreta 2 (Pergunta ${i})" required placeholder="Resposta incorreta 2">
+                    <input type="url" name="URL da Imagem Incorreta 2 (Pergunta ${i})" required placeholder="URL da imagem 2">
+                </div>
+
+                <div class="setup questions">
+                    <input type="text" name="Resposta Incorreta 3 (Pergunta ${i})" required placeholder="Resposta incorreta 3">
+                    <input type="url" name="URL da Imagem Incorreta 3 (Pergunta ${i})" required placeholder="URL da imagem 3">
+                </div>
+
+            </div>`
+    }
+
+    questionsLocale.innerHTML += `<button onclick="verifyLevels()">Prosseguir pra criar níveis</button>`;
+}
+
+getQuizzes();
